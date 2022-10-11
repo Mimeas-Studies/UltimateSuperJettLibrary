f44a604d7e3fe66c0247e9676a352b70dd92f28b
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-11 19:50:34 +0200

    Added logevents as logger and shifted server start message to logger

--- src/main/java/com/github/mimeas_studies/usjlib/LibraryServer.java
+++ src/main/java/com/github/mimeas_studies/usjlib/LibraryServer.java
@@ -1,17 +1,27 @@
 package com.github.mimeas_studies.usjlib;
 
 import org.eclipse.jetty.server.Server;
-import java.net.InetSocketAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LibraryServer {
 
+    private final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
     private final Server server;
 
     public LibraryServer(int port) {
-        server = new Server(new InetSocketAddress("127.0.0.1", port));
+        server = new Server(port);
+    }
+
+    public URL getURL() throws MalformedURLException {
+        return server.getURI().toURL();
     }
 
     public void start() throws Exception {
         server.start();
+        logger.info("Started server at {}", getURL());
     }
 }
