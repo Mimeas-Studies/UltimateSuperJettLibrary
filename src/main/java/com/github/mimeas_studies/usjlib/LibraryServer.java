e13fe4d978dbce8ec9e46f4fd40906096c91c554
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2022-10-25 13:07:32 +0200

    Changed location the servers serves from when developing

--- src/main/java/com/github/mimeas_studies/usjlib/LibraryServer.java
+++ src/main/java/com/github/mimeas_studies/usjlib/LibraryServer.java
@@ -1,12 +1,15 @@
 package com.github.mimeas_studies.usjlib;
 
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,17 +18,34 @@ public class LibraryServer {
     private final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
     private final Server server;
 
-    public LibraryServer(int port) {
+    public LibraryServer(int port) throws IOException {
         server = new Server(port);
         server.setHandler(createWebApp());
     }
 
-    private WebAppContext createWebApp() {
+    private WebAppContext createWebApp() throws IOException {
         WebAppContext webapp = new WebAppContext();
         webapp.setContextPath("/");
 
         Resource resource = Resource.newClassPathResource("/webapp");
-        webapp.setBaseResource(resource);
+        var filepath = new File(
+                resource.getFile()
+                        .getAbsoluteFile()
+                        .toString()
+                        .replace("\\", "/")
+                        .replace("target/classes", "src/main/resources")
+        );
+        if (filepath.isDirectory()) {
+            webapp.setBaseResource(Resource.newResource(filepath));
+            webapp.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        }
+        else {
+            webapp.setBaseResource(resource);
+        }
+
+        var apiServlet = webapp.addServlet(ServletContainer.class, "/api/*");
+        webapp.setInitParameter("jersey.config.server.provider.packages", "com.github.mimeas_studies.usjlib");
+
 
         return webapp;
     }
